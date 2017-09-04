fd5c0c295affdb14c4172a116b07fe2435876183
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-04 19:00:28 +0700

    javascript

--- a.js
+++ a.js
@@ -1,4 +1,10 @@
-console.log('1223');
-console.log('1223');
-console.log('1223');
-console.log('1223');
+// ES6 
+let a = 'Hello world';
+const b = 'Xin chao';
+// var c = 'iqeh3efsdf';
+// d = '92ef8e8hdfd';
+
+a = 'MEAN';
+a = 1;
+// b = 'Hello';
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
